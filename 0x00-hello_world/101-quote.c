7f102bb2f128cab8fb1c574893cf67f48110050b
e-opti <ennamani85@gmail.com>
2023-02-16 10:11:03 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-char fls[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+char fls[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 fwrite(fls, sizeof(fls) - 1, 1, stderr);
 return (1);
 }
