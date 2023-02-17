c7d12cf9f36113d1a9c4f8549d5d894e4a135706
e-opti <ennamani85@gmail.com>
2023-02-17 13:09:58 -0800

    single digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - single digits
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	char dg;
+
+	for (dg = 0; dg < 10; dg++)
+	{
+		putchar(dg);
+	}
+	putchar('\n');
+
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -3,3 +3,4 @@ last digit
 print lowercase alphabet
 lower and upper
 except q and e
+single digit
