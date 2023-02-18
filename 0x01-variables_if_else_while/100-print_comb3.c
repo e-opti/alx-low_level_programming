9b9f4392cd980228c9b9e39e5f7f4a0125ce522a
e-opti <ennamani85@gmail.com>
2023-02-17 16:24:55 -0800

    double combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,14 +10,15 @@ int main(void)
 	int tens;
 	int ones;
 
-	for (tens = '0'; tens <= '9'; tens++)
+	for (tens = 0; tens <= 9; tens++)
 	{
-		for (ones = tens + '1'; ones <= '9'; ones++)
+		for (ones = tens + 1; ones <= 10; ones++)
 		{
 			putchar(tens);
 			putchar(ones);
 
-			if (tens != '8' || ones != '9')
+			if (tens == '8' && ones == '9')
+				continue;
 			{
 				putchar(',');
 				putchar(' ');
