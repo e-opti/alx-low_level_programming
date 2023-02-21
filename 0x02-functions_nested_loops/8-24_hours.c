93220a49831b14e8a97423dcc8bc4f3196ebd9eb
e-opti <ennamani85@gmail.com>
2023-02-21 12:42:04 -0800

    24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,15 +11,13 @@ void jack_bauer(void)
 
 	for (hr = 0; hr < 24; hr++)
 	{
-		_putchar('0' + (hr / 10));
-		_putchar('0' + (hr % 10));
-	}
-	_putchar('\n');
-
-	for (min = 0; min < 60; min++)
-	{
-		_putchar('0' + (min / 10));
-		_putchar('0' + (min % 10));
+		for (min = 0; min < 60; min++)
+		{
+			_putchar('0' + (hr / 10));
+			_putchar('0' + (hr % 10));
+			_putchar('0' + (min / 10));
+			_putchar('0' + (min % 10));
+		}
 	}
 	_putchar('\n');
 }
