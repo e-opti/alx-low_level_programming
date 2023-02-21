8fa5ea27252f33aa1d1925e3f2f45fc0cf17c13f
e-opti <ennamani85@gmail.com>
2023-02-21 04:52:31 -0800

    lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,6 @@
 /**
  *print_alphabet - lowercase alphabet
  *
- *Return: 0
  */
 void print_alphabet(void)
 {
@@ -14,6 +13,4 @@ void print_alphabet(void)
 		_putchar(alp);
 	}
 	_putchar('\n');
-
-	return (0);
 }
