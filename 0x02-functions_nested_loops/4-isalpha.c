2497fde709b2fc760b07e0b0cfba848e130feadb
e-opti <ennamani85@gmail.com>
2023-02-21 06:38:11 -0800

    is alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_isalpha - is alphabet
+ *
+ *@c: check the letter
+ *
+ *Return: 1 or 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z', c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+	_putchar('\n');
+}
