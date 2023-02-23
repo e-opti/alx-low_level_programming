50a53539cb6be09b3cf6218d06e23b99218e2358
e-opti <ennamani85@gmail.com>
2023-02-23 14:11:49 -0800

    more num

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *more_numbers - 0 - 14, 10x
+ */
+void more_numbers(void)
+{
+	int c, d;
+
+	for (c = '0'; c <= '14'; c++)
+	{
+		for (d = 1; d <= 10; d++)
+		{
+			_putchar(d);
+		}
+		_putchar('\n');
+	}
+}
